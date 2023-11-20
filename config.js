d112c13757917c6774963cff699b569a69a1c1e0
tw31122007 <tw31122007@googlemail.com>
2023-11-21 09:28:53 +1100

    1

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-proxy.tw31122007.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2FhZjVmNDU5OThlNDUwZTc4M2JjNjNiOWQ0NmM4OCIsInN1YiI6IjY0ZTZlNmJhYzYxM2NlMDEyY2M0NWY3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kNVfK69J_wLFxtDZDc3BBkiKDZmdLCf9RrVdKWuuu4k"
+};
